ad9df57dc181d71844aad49a6e8cb7a20308ef7d
Bhaskar1221 <145801462+Bhaskar1221@users.noreply.github.com>
2025-02-23 17:25:23 +0000

    done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345;
+let accountEmail = "bhaskar@gmail.com";
+var accountPassword = 2345;
+accountCity = "Haldwani";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+accountEmail = 'fulpatiya@gmail.com'
+accountPassword = 12305
+accountCity = 'Nainital'
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// Do not use var coz of issue in Block scope and Functional Scope
