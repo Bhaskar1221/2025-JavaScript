0e4003d174a81c43c2b0982590ecddcf11815054
Bhaskar1221 <145801462+Bhaskar1221@users.noreply.github.com>
2025-02-24 05:18:21 +0000

    Done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all code as newer version
+
+console.log(3
+    +3)      //code readability should be high
+
+let name = "Bhaskar"
+let age = 23
+let isLoggedIn = false
+
+/* number  = 2^53
+   bigInt 
+   String = ""
+   boolean = true /false
+   null = standalone value
+   undefined = 
+   symbol= unique
+*/
+
+//object
+
+console.log(typeof isLoggedIn) // boolean
+console.log(typeof null)      // object
+console.log(typeof undefined) // undefined
