c50890f6c6e99bf178d3084a1d9ac74cb595344d
Bhaskar1221 <145801462+Bhaskar1221@users.noreply.github.com>
2025-05-13 06:21:59 +0000

    done

--- 01_Basics/03_datatypeconversion.js
+++ 01_Basics/03_datatypeconversion.js
@@ -1,10 +1,10 @@
 let number = "33abc"
-console.log(typeof number);
-console.log(typeof(number));
+//console.log(typeof number);
+//console.log(typeof(number));
 
 let isnumber = Number(number)
-console.log(typeof number);
-console.log(isnumber);
+//console.log(typeof number);
+//console.log(isnumber);
 
 
 // "33">= 33
@@ -14,7 +14,7 @@ console.log(isnumber);
 let islogged = 1
 
 let isboolean = Boolean(islogged)
-console.log(isboolean);
+//console.log(isboolean);
 
 /* 1>= true , 0>= false
 "">= false, "bhaskar">=true. */
@@ -22,4 +22,53 @@ console.log(isboolean);
 let num = 22
 
 let stringnumber = String(num)
-console.log(typeof(stringnumber));
+//console.log(typeof(stringnumber));
+
+
+/*-------------------------------------Operations-------------------------------------*/
+
+let value = 2
+let negvalue = -value
+//console.log(negvalue);
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2%3);
+//console.log(2**3);
+
+
+let str1 ="Hi"
+let str2 = "Bhaskar"
+let str3 = str1 + " "+ str2
+//console.log(str3);
+
+//console.log("1" + 2)
+//console.log(1 + "2");
+//console.log("1" +2 +2);
+//console.log(1 + 2 + "2");
+
+//console.log(+true);
+//console.log(+"");
+
+let gamecounter = 100
+gamecounter++
+//console.log(gamecounter);
+
+let x = 4
+//let y = x++
+//console.log(`x=${x}, y=${y}`);
+
+let a = 4
+let b = a--
+//console.log(`a=${a}, b=${b}`);
+
+let quantity = -2;
+while (quantity < 5) {
+  quantity = Math.max(0, ++quantity); // Increment quantity first, then ensure it's not negative
+  console.log(`Current quantity: ${quantity}`);
+}
+
+
+
+
