4fe32bb0abffe6f8832765a436ad5260df91cf89
Bhaskar1221 <145801462+Bhaskar1221@users.noreply.github.com>
2025-05-13 04:55:37 +0000

    done

---
+++ 01_Basics/03_datatypeconversion.js
@@ -0,0 +1,25 @@
+let number = "33abc"
+console.log(typeof number);
+console.log(typeof(number));
+
+let isnumber = Number(number)
+console.log(typeof number);
+console.log(isnumber);
+
+
+// "33">= 33
+// "33abc">= NaN
+// True>= 1 and false>= 0
+
+let islogged = 1
+
+let isboolean = Boolean(islogged)
+console.log(isboolean);
+
+/* 1>= true , 0>= false
+"">= false, "bhaskar">=true. */
+
+let num = 22
+
+let stringnumber = String(num)
+console.log(typeof(stringnumber));
