967c384c3657d75ae023469ccf0772aa3382b9a6
Bhaskar1221 <145801462+Bhaskar1221@users.noreply.github.com>
2025-05-13 06:41:38 +0000

    done

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,15 @@
+//console.log("2">1); //JS convert string into number and give result
+
+/* console.log(null>0);
+console.log(null==0);
+console.log(null>=0); */
+
+/* equality operator and comparision operator works differently ,comparision convert null into a number treating as 0 */
+
+//console.log(undefined==0);
+//console.log(undefined>0);
+//console.log(undefined>=0);
+
+// === [it check strictly means check both value and data type]
+
+console.log("2"===2);
